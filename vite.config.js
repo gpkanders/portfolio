0e611ffa4ede0d2bf5c576079af70866cbf1b6ed
gpkanders <158122964+gpkanders@users.noreply.github.com>
2026-07-12 12:00:04 -0700

    update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,12 +3,14 @@ import react, { reactCompilerPreset } from '@vitejs/plugin-react'
 import babel from '@rolldown/plugin-babel'
 
 import tailwindcss from '@tailwindcss/vite'
+import { imagetools } from 'vite-imagetools';
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
     react(),
     babel({ presets: [reactCompilerPreset()] }),
-    tailwindcss()
+    tailwindcss(),
+    imagetools()
   ],
 })
